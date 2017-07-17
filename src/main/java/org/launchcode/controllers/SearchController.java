b8ce2b009c3e6415834f890b657dc94f6c24bed0
Melissa Green <melissabgreen31@gmail.com>
2017-07-17 11:50:43 -0500

    Finished MVC

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -1,11 +1,14 @@
 package org.launchcode.controllers;
 
 import org.launchcode.models.JobData;
+import org.springframework.boot.autoconfigure.batch.BatchProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -23,5 +26,20 @@ public class SearchController {
     }
 
     // TODO #1 - Create handler to process search request and display results
+
+    @RequestMapping(value = "results", method = RequestMethod.GET)
+    public String processJobSearch(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        model.addAttribute("columns", ListController.columnChoices);
+        ArrayList<HashMap<String, String>> jobs;
+
+        if (searchType.equals("all")) {
+            jobs = JobData.findByValue(searchTerm);
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+
+        model.addAttribute("jobs", jobs);
+    return "search";
+    }
 
 }
